d68c9280bc66d5eef8ac501786532ff14397a98a
Kenn <arvin.delossantos@clickplaycorp.com>
2020-04-11 20:47:05 +0800

    Updates

---
+++ precache-manifest.708039eb494329793c64a2621e83b687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9666847dcbe972e14915b0f5b765241a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce0c3f8623a9e731ebc3",
+    "url": "/static/css/2.b9b2dd63.chunk.css"
+  },
+  {
+    "revision": "9cd976800f2cedb4fce2",
+    "url": "/static/css/main.59950aa6.chunk.css"
+  },
+  {
+    "revision": "ce0c3f8623a9e731ebc3",
+    "url": "/static/js/2.b5d9879d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b5d9879d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd976800f2cedb4fce2",
+    "url": "/static/js/main.876cfbb4.chunk.js"
+  },
+  {
+    "revision": "03b293c596ce2613f7f3",
+    "url": "/static/js/runtime-main.7d3cc347.js"
+  }
+]);
